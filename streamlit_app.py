7eaeb4bd944a8cb59d5455e5c479b45623ce772e
Alexandre Berger <alexandre.berger@citalid.com>
2025-11-28 23:15:04 +0100

    feature: connecting to db

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,19 @@
 import streamlit as st
+import psycopg2
 
-# Série
-exercises = st.multiselect("Exercise:", ["Curl", "Bench Press"])
+cfg = st.secrets["neon"]
+conn = psycopg2.connect(
+    host=cfg["host"],
+    dbname=cfg["dbname"],
+    user=cfg["user"],
+    password=cfg["password"],
+    sslmode="require"
+)
+cursor = conn.cursor()
+
+# --- UI ---
+st.write("Track down a series:")
+exercise = st.selectbox("Exercise:", ["Curl", "Bench Press"])
 weight = st.number_input("Weight: ", min_value=0)
 reps = st.number_input("Reps: ", min_value=0)
-reps_in_reserve = st.number_input("RIR: ", min_value=0)
+rir = st.number_input("RIR: ", min_value=0)
