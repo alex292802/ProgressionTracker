93d36162fd6964d91fd875682945d9e6117a7ca9
Alexandre Berger <alexandre.berger@citalid.com>
2025-11-28 23:28:34 +0100

    feature: retrieving exercices from database

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,9 +11,15 @@ conn = psycopg2.connect(
 )
 cursor = conn.cursor()
 
+cursor.execute("SELECT name FROM exercice")
+exercises_list = [row[0] for row in cursor.fetchall()]
+
 # --- UI ---
 st.write("Track down a series:")
-exercise = st.selectbox("Exercise:", ["Curl", "Bench Press"])
+exercise = st.selectbox("Exercise:", exercises_list)
 weight = st.number_input("Weight: ", min_value=0)
 reps = st.number_input("Reps: ", min_value=0)
 rir = st.number_input("RIR: ", min_value=0)
+
+cursor.close()
+conn.close()
