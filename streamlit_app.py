d1293ccd17ad1aa9176e15099cc999df787e4b5a
Alexandre Berger <alexandre.berger@citalid.com>
2025-11-28 22:57:12 +0100

    feature: basic components

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,7 @@
-print("hey")
+import streamlit as st
+
+# Série
+exercises = st.multiselect("Exercise:", ["Curl", "Bench Press"])
+weight = st.number_input("Weight: ", min_value=0)
+reps = st.number_input("Reps: ", min_value=0)
+reps_in_reserve = st.number_input("RIR: ", min_value=0)
