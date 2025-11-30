3ba06f48a1c74a3c1448087f9271eb19f358df01
Alexandre Berger <alexandre.berger@citalid.com>
2025-11-30 21:45:01 +0100

    fix: structure

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,51 +12,53 @@ conn = psycopg2.connect(
 )
 cursor = conn.cursor()
 
-# TODO: better structure for user (for the moment, it supposes that user names are unique)
-cursor.execute("SELECT id, name FROM app_user")
-users = cursor.fetchall()
 st.title("Progression Tracker")
-current_user = st.selectbox("Je suis:", [t[1] for t in users])
-user_id = next(u[0] for u in users if u[1] == current_user)
 
-cursor.execute(
-    "SELECT id FROM training WHERE end_time IS NULL AND user_id = %s",
-    (user_id,)
-)
-on_going_trainings_ids = [row[0] for row in cursor.fetchall()]
-
-if len(on_going_trainings_ids) == 0:
-    st.session_state.training_id = None
+if st.session_state.user_id is None:
+    # TODO: better structure for user (for the moment, it supposes that user names are unique)
+    cursor.execute("SELECT id, name FROM app_user")
+    users = cursor.fetchall()
+    current_user = st.selectbox("Je suis:", [t[1] for t in users])
+    user_id = next(u[0] for u in users if u[1] == current_user)
 else:
-    st.session_state.training_id = on_going_trainings_ids[0]
-
-if st.session_state.training_id is None:
-    cursor.execute("SELECT id, name FROM training_type")
-    training_types = cursor.fetchall()
-    selected_training = st.selectbox(
-        "Type d'entrainement :",
-        [t[1] for t in training_types]
+    cursor.execute(
+        "SELECT id FROM training WHERE end_time IS NULL AND user_id = %s",
+        (st.session_state.user_id,)
     )
-    if st.button("Commencer mon entrainement"):
-        training_type_id = next(t[0] for t in training_types if t[1] == selected_training)
-        cursor.execute(
-            """
-            INSERT INTO training (start_time, user_id, training_type_id)
-            VALUES (%s, %s, %s)
-            RETURNING id
-            """,
-            (datetime.now(), user_id, training_type_id)
+    on_going_trainings_ids = [row[0] for row in cursor.fetchall()]
+
+    if len(on_going_trainings_ids) == 0:
+        st.session_state.training_id = None
+    else:
+        st.session_state.training_id = on_going_trainings_ids[0]
+
+    if st.session_state.training_id is None:
+        cursor.execute("SELECT id, name FROM training_type")
+        training_types = cursor.fetchall()
+        selected_training = st.selectbox(
+            "Type d'entrainement :",
+            [t[1] for t in training_types]
         )
-        st.session_state.training_id = cursor.fetchone()[0]
-        conn.commit()
-else:
-    cursor.execute("SELECT name FROM exercice")
-    exercises_list = [row[0] for row in cursor.fetchall()]
-    st.write("Track down a series:")
-    exercise = st.selectbox("Exercise:", exercises_list)
-    weight = st.number_input("Poids: ", min_value=0)
-    reps = st.number_input("Reps: ", min_value=0)
-    rir = st.number_input("RIR: ", min_value=0)
+        if st.button("Commencer mon entrainement"):
+            training_type_id = next(t[0] for t in training_types if t[1] == selected_training)
+            cursor.execute(
+                """
+                INSERT INTO training (start_time, user_id, training_type_id)
+                VALUES (%s, %s, %s)
+                RETURNING id
+                """,
+                (datetime.now(), st.session_state.user_id, training_type_id)
+            )
+            st.session_state.training_id = cursor.fetchone()[0]
+            conn.commit()
+    else:
+        cursor.execute("SELECT name FROM exercice")
+        exercises_list = [row[0] for row in cursor.fetchall()]
+        st.write("Track down a series:")
+        exercise = st.selectbox("Exercise:", exercises_list)
+        weight = st.number_input("Poids: ", min_value=0)
+        reps = st.number_input("Reps: ", min_value=0)
+        rir = st.number_input("RIR: ", min_value=0)
 
 cursor.close()
 conn.close()
