2cb58b5232ab7c54dcc0e119e25c307b5d037562
alex292802 <alexandre.berger2802@gmail.com>
2025-12-13 22:34:20 +0100

    refactor: adding series file

---
+++ series.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+def add_series(cursor, conn, training_id):
+    cursor.execute("SELECT id, name FROM exercice")
+    exercises = cursor.fetchall()
+    st.subheader("Ajouter une série")
+    exercise = st.selectbox("Exercice:", exercises, format_func=lambda e: e[1])
+    weight = st.number_input("Poids:", min_value=0)
+    reps = st.number_input("Reps:", min_value=0)
+    rir = st.number_input("RIR:", min_value=0)
+
+    if st.button("Ajouter la série"):
+        cursor.execute(
+            """
+            INSERT INTO series (training_id, exercice_id, weight, reps, rir, created_at)
+            VALUES (%s, %s, %s, %s, %s, %s)
+            """,
+            (training_id, exercise[0], weight, reps, rir, datetime.now())
+        )
+        conn.commit()
+        st.success("Série ajoutée avec succès !")
--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 from training import render_training_recap, get_ongoing_training_id, start_new_training, select_past_training
 from user import select_user
+from series import add_series
 
 cfg = st.secrets["neon"]
 conn = psycopg2.connect(
@@ -36,26 +37,7 @@ else:
         else:
             render_training_recap(cursor, st.session_state.shown_training_id)
     else:
-        cursor.execute("SELECT id, name FROM exercice")
-        exercises_list = cursor.fetchall()
-        st.write("Ajouter une série:")
-        exercise = st.selectbox("Exercice:", options=exercises_list, format_func=lambda e: e[1])
-        weight = st.number_input("Poids: ", min_value=0)
-        reps = st.number_input("Reps: ", min_value=0)
-        rir = st.number_input("RIR: ", min_value=0)
-
-        if st.button("Ajouter la série"):
-            exercise_id = exercise[0]
-            cursor.execute(
-                """
-                INSERT INTO series (training_id, exercice_id, weight, reps, rir, created_at)
-                VALUES (%s, %s, %s, %s, %s, %s)
-                """,
-                (st.session_state.training_id, exercise_id, weight, reps, rir, datetime.now())
-            )
-            conn.commit()
-            st.success("Série ajoutée avec succès !")
-
+        add_series(cursor, conn, st.session_state.training_id)
         if st.button("Terminer le training"):
             cursor.execute(
                 "UPDATE training SET end_time = %s WHERE id = %s",
